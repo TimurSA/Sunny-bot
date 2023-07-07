d41735e764f359bc722a18ff77d0924583e1c7c8
TimurSA <sayranov.timur@gmail.com>
2023-07-07 14:38:52 +0500

    inital commit

---
+++ handlers/handler.py
@@ -0,0 +1,21 @@
+# импортируем библиотеку abc для реализации абстрактных классов
+from abc import ABCMeta, abstractmethod
+# импортируем разметку клавиатуры и клавиш
+from markup.markup import Keyboards
+# импортируем класс-менеджер для работы с библиотекой
+from data_base.dbalchemy import DBManager
+
+
+class Handler(metaclass=ABCMeta):
+
+    def __init__(self, bot):
+        # получаем объект бота
+        self.bot = bot
+        # инициализируем разметку кнопок
+        self.keybords = Keyboards()
+        # инициализируем менеджер для работы с БД
+        self.BD = DBManager()
+
+    @abstractmethod
+    def handle(self):
+        pass
